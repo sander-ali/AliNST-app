e784723bbca871b731a261d1172c4dc51163b704
Dr. Sunder Ali Khowaja <sandar.ali@gmail.com>
2022-01-28 15:53:37 +0900

    app files

---
+++ main.py
@@ -0,0 +1,51 @@
+#First try for developing web server
+from flask import Flask, request, render_template, send_from_directory, redirect, send_file
+import os
+import test
+import neuralStyleProcess
+import cv2
+
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+@app.route("/")
+def index():
+    return render_template("upload.html")
+
+@app.route("/upload", methods=['POST'])
+def upload():
+	target = os.path.join(APP_ROOT, 'images\\')
+	print("TARGET", target)
+
+	if not os.path.isdir(target):
+		os.mkdir(target)
+	else:
+		print("Couldn't create upload directory: {}".format(target))
+
+	data = request.form.get("style")
+
+	myFiles = []
+
+	for file in request.files.getlist("file"):
+		filename = file.filename
+		destination = "".join([target, filename])
+		file.save(destination)
+		myFiles.append(filename)
+
+	return render_template("complete.html", image_names=myFiles, selected_style=data)
+
+@app.route('/upload/<filename>')
+def send_original_image(filename):
+    return send_from_directory("images", filename)
+
+@app.route('/complete/<filename>/<selected_style>')
+def send_processed_image(filename, selected_style):
+    directoryName = os.path.join(APP_ROOT, 'images\\')
+    
+    newImg = neuralStyleProcess.neuralStyleTransfer(directoryName, filename, selected_style)
+    
+    return send_from_directory("images", newImg)
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
---
+++ neuralStyleProcess.py
@@ -0,0 +1,38 @@
+import imutils
+import cv2
+import os
+import numpy as np
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+def neuralStyleTransfer(directoryName, filename, selected_style):
+    target = os.path.join(APP_ROOT, 'static/models/')
+    net = cv2.dnn.readNetFromTorch(target + selected_style)
+    
+    image = cv2.imread(directoryName+filename)
+    image = imutils.resize(image, width=600)
+    (h,w) = image.shape[:2]
+    
+    blob = cv2.dnn.blobFromImage(image, 1.0, (w,h), (103.939, 116.779, 123.680), swapRB=False, crop=False)
+    net.setInput(blob)
+    
+    output = net.forward()
+    
+    output = output.reshape((3,output.shape[2], output.shape[3]))
+    output[0] += 103.939
+    output[1] += 116.779
+    output[2] += 123.680
+    
+    output = output.transpose(1,2,0)
+    
+    filename, file_extension = os.path.splitext(filename)
+    print(filename)
+    newFileName = 'processedImg'+'_'+ filename + file_extension
+    cv2.imwrite(directoryName + newFileName, output)
+    print(newFileName)
+    print(directoryName)
+    
+    return newFileName
+
+if __name__ == '__main__':
+    neuralStyleTransfer('/Users/sandarali.usindh/Documents/pyFlaskCV/images', 'bbq.jpg', 'gold_black_2700.t7')
